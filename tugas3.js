d2d26f74b8c7c0ffb88be3c7b825b0e98a95140c
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 05:51:52 +0700

    tugas 3 JS
    tugas untuk Niomic

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+// 1. Buat function dengan nilai return dan parameter
+function namaFunction(a) {
+	// 3. di dalam function ubah nilai dari variabel
+	var a = 'ubah';
+	// 4. tampilkan
+	console.log(a);
+	return a;
+}
+// 2. kirim sebuah nilai
+// 4. tampilkan
+console.log(namaFunction(12));
